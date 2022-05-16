95d05ecf9f1740ae61036eadceeeaf7f4129a97d
antoinedelia <antoine.delia@gmail.com>
2022-05-16 23:25:39 +0200

    Upgrade providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.6.0"
+      version = "4.14.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.2"
+      version = "3.1.3"
     }
   }
 }

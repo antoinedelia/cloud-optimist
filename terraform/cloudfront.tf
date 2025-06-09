9435e9cf031e130107e2d26aa53b4e71cf2f4504
Antoine Delia <antoine.delia@gmail.com>
2025-06-09 13:08:28 +0200

    chore: add security headers

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,3 +1,40 @@
+resource "aws_cloudfront_response_headers_policy" "static_site_security" {
+  name = "StaticSiteSecurityHeaders"
+
+  security_headers_config {
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+
+    content_security_policy {
+      content_security_policy = "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+      override                = true
+    }
+
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+
+    content_type_options {
+      override = true
+    }
+
+    referrer_policy {
+      referrer_policy = "strict-origin-when-cross-origin"
+      override         = true
+    }
+
+    permissions_policy {
+      permissions_policy = "geolocation=(), microphone=(), camera=()"
+      override           = true
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "dist" {
   origin {
     domain_name = aws_s3_bucket.site.website_endpoint
@@ -9,6 +46,7 @@ resource "aws_cloudfront_distribution" "dist" {
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
+
   enabled             = true
   default_root_object = "index.html"
 
@@ -24,17 +62,21 @@ resource "aws_cloudfront_distribution" "dist" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = aws_s3_bucket.site.id
+
     forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+
+    viewer_protocol_policy     = "redirect-to-https"
+    compress                   = true
+    min_ttl                    = 0
+    default_ttl                = 3600
+    max_ttl                    = 86400
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.static_site_security.id
   }
 
   viewer_certificate {
